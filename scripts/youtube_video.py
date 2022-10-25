000cd40824b88c9c439e8a5e4b01f700a2df7998
vladtr3 <92353919+trezev@users.noreply.github.com>
2022-10-25 14:12:44 +0600

    Add files via upload

--- scripts/youtube_video.py
+++ scripts/youtube_video.py
@@ -1,2 +1,3 @@
 from pytube import YouTube # to install pytube: python -m pip install pytube
 YouTube(input("\n\nYOUTUBE VIDEO DOWLOADER\nurl: ")).streams.get_highest_resolution().download() # the file will be saved in the folder where the script is located
+print("\n\nSuccessfully downloaded\n\n") # print "Successfully downloaded" when file dowloaded
