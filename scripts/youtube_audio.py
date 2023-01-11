9553baed6bf6010e51aec5cb0de8f67f2dc5cb52
vladtr3 <92353919+trezev@users.noreply.github.com>
2023-01-11 17:28:07 +0600

    Delete scripts directory

--- scripts/youtube_audio.py
+++
@@ -1,7 +0,0 @@
-import os # for work with files
-from pytube import YouTube # to install pytube: python -m pip install pytube
-audio_get = YouTube(str(input("\n\nYOUTUBE AUDIO DOWLOADER\nurl: "))).streams.filter(only_audio=True).first().download() # the file will be saved in the folder where the script is located
-base, ext = os.path.splitext(audio_get) # get file name
-audio_out = base + ".mp3" # get original file name + mp3
-os.rename(audio_get, audio_out) # rename the audio_get to audio_out
-print("\n\nSuccessfully downloaded\n\n") # print "Successfully downloaded" when file dowloaded
--- scripts/youtube_video.py
+++
@@ -1,3 +0,0 @@
-from pytube import YouTube # to install pytube: python -m pip install pytube
-YouTube(input("\n\nYOUTUBE VIDEO DOWLOADER\nurl: ")).streams.get_highest_resolution().download() # the file will be saved in the folder where the script is located
-print("\n\nSuccessfully downloaded\n\n") # print "Successfully downloaded" when file dowloaded
